9a0652874157791b377eecef7629ad30333da836
DavidTranMinhAnh <davidtran31770@gmail>
2024-11-13 10:36:42 +0100

    updated code

---
+++ Code/PHP/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Accueil</title>
+</head>
+
+<body>
+
+    <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+        <path fill="#FF0066" d="M39.3,-50.9C52.3,-44.6,65.2,-35,68,-22.9C70.9,-10.8,63.8,3.8,60.4,21.2C56.9,38.6,57.1,58.7,47.6,64.2C38.2,69.7,19.1,60.5,5.9,52.5C-7.4,44.4,-14.8,37.4,-26.6,32.7C-38.5,28,-55,25.6,-64.7,16.2C-74.5,6.7,-77.6,-9.7,-69.7,-19.2C-61.9,-28.7,-43.2,-31.2,-29.7,-37.3C-16.3,-43.4,-8.1,-53.1,2.5,-56.5C13.1,-60,26.3,-57.1,39.3,-50.9Z" transform="translate(100 100)" />
+    </svg>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
