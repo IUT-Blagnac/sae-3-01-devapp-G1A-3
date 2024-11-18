27994496390c99aa5f187f077ba7c6073959223c
DavidTRANMinhAnh <126153957+DavidTRANMinhAnh@users.noreply.github.com>
2024-11-18 16:24:30 +0100

    Delete Code/PHP/include directory

--- Code/PHP/include/header.php
+++
@@ -1,6 +0,0 @@
-<body class="d-flex flex-column min-vh-100">
-    <header class="bg-primary text-white text-center py-1">
-        <div class="container">
-            <h1>SweetShops</h1>
-        </div>
-    </header>
