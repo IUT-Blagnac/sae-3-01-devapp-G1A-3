d8f0c1998fa069586931a20f4cb8f42aeebfd322
DavidTRANMinhAnh <126153957+DavidTRANMinhAnh@users.noreply.github.com>
2024-11-18 16:26:21 +0100

    Delete Code/PHP/SupprProd.php

--- Code/PHP/SupprProd.php
+++
@@ -1,5 +0,0 @@
-<?php require_once("./include/verif_session.php");
-      require_once('connect.inc.php');
-      $result = $conn -> prepare ("DELETE FROM Produits WHERE idProduit = ?");
-      $result -> execute(array(htmlentities($_GET["idProduit"])));
-?>
