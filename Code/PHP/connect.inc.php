357ee5ac6684047c2d84023e4419d1ee2d5e07f0
NariaReynhard <84771519+NariaReynhard@users.noreply.github.com>
2024-11-18 16:40:19 +0100

    connect.inc.php branché sur BD test
    Changement des logins dans le connect.inc.php pour connecter à un environnement PhpMyAdmin avec une base de donnée en attendant l'environnement fourni pour la SAE

--- Code/PHP/connect.inc.php
+++ Code/PHP/connect.inc.php
@@ -1,11 +1,9 @@
-<?php
-$user = 'R2024PHP3009';
-$passwd = 'P224e8NV3qUbwr';
-
+<?php 
 try{
-    $conn = new PDO("mysql:host=127.0.0.1;dbname=R2024PHP3009;charset=UTF8", $user, $passwd);
-}catch(PDOException $e){
-    echo "Erreur de connexion :".$e->getMessage()."<br>";
-    die;
+  $connect = new PDO ('mysql:host=localhost;dbname=R2024PHP3011;charset=UTF8','R2024PHP3011','37ewGVpb89Mv5X');
+}
+catch (PDOException $e) {
+  echo "Erreur : ".$e->getMessage()."<br/>";
+  die();
 }
 ?>
