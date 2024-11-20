86814115f9d293a2ab7220c80181fd988c68e55c
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-11-20 16:14:19 +0100

    Gros avancement Affichages/graphiques

--- Code/java/src/main/java/application/view/DonneesAnterieuresMultiplesController.java
+++ Code/java/src/main/java/application/view/DonneesAnterieuresMultiplesController.java
@@ -26,7 +26,9 @@ public class DonneesAnterieuresMultiplesController {
 	}
 
     public void displayDialog(){
-        lalala(grapheTemp);
+        donneesTemperature(grapheTemp);
+        donneesHumidite(grapheHum);
+        donneesCO2(grapheCO2);
         this.containingStage.show();
     }
 
@@ -50,7 +52,7 @@ public class DonneesAnterieuresMultiplesController {
         main.changementAnterieur(containingStage);
     }
 
-    public void lalala(LineChart<Number, Number> graph){
+    public void donneesTemperature(LineChart<Number, Number> graph){
         //Définir l'axe X
         NumberAxis xAxis = new NumberAxis(0, 24, 1); 
         xAxis.setLabel("Heure de la journée"); 
@@ -79,8 +81,64 @@ public class DonneesAnterieuresMultiplesController {
         graph.getData().clear();  // Assurez-vous de vider les anciennes données si nécessaire
         graph.getData().add(series);
     }
-    
 
-    /*AlertUtilities.showAlert(this.containingStage, "Erreur clôture compte", null, "Le compte doit avoir un solde égal à 0 pour être clôturé",
-	AlertType.WARNING);*/
+    public void donneesHumidite(LineChart<Number, Number> graph){
+        //Définir l'axe X
+        NumberAxis xAxis = new NumberAxis(0, 24, 1); 
+        xAxis.setLabel("Heure de la journée"); 
+                
+        //Définir l'axe Y
+        NumberAxis yAxis = new NumberAxis(0, 40, 5); 
+        yAxis.setLabel("Humidité");
+    
+        //Assurez-vous que le graphique passé (graph) utilise les bons axes
+        graph.getXAxis().setLabel(xAxis.getLabel());
+        graph.getYAxis().setLabel(yAxis.getLabel());
+    
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        series.setName("Pourcentage d'humidité en fonction de l'heure de la journée");
+    
+        // Ajouter des données au graphique
+        series.getData().add(new XYChart.Data<>(3, 60));
+        series.getData().add(new XYChart.Data<>(6, 58));
+        series.getData().add(new XYChart.Data<>(9, 52));
+        series.getData().add(new XYChart.Data<>(12, 44));
+        series.getData().add(new XYChart.Data<>(15, 41));
+        series.getData().add(new XYChart.Data<>(18, 37));
+        series.getData().add(new XYChart.Data<>(21, 44));
+    
+        // Ajouter les séries de données au graphique passé en paramètre
+        graph.getData().clear();  // Assurez-vous de vider les anciennes données si nécessaire
+        graph.getData().add(series);
+    }
+
+    public void donneesCO2(LineChart<Number, Number> graph){
+        //Définir l'axe X
+        NumberAxis xAxis = new NumberAxis(0, 24, 1); 
+        xAxis.setLabel("Heure de la journée"); 
+                
+        //Définir l'axe Y
+        NumberAxis yAxis = new NumberAxis(0, 40, 5); 
+        yAxis.setLabel("CO2");
+    
+        //Assurez-vous que le graphique passé (graph) utilise les bons axes
+        graph.getXAxis().setLabel(xAxis.getLabel());
+        graph.getYAxis().setLabel(yAxis.getLabel());
+    
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        series.setName("Taux de CO2 en fonction de l'heure de la journée");
+    
+        // Ajouter des données au graphique
+        series.getData().add(new XYChart.Data<>(3, 300));
+        series.getData().add(new XYChart.Data<>(6, 300));
+        series.getData().add(new XYChart.Data<>(9, 800));
+        series.getData().add(new XYChart.Data<>(12, 1200));
+        series.getData().add(new XYChart.Data<>(15, 1100));
+        series.getData().add(new XYChart.Data<>(18, 1000));
+        series.getData().add(new XYChart.Data<>(21, 500));
+    
+        // Ajouter les séries de données au graphique passé en paramètre
+        graph.getData().clear();  // Assurez-vous de vider les anciennes données si nécessaire
+        graph.getData().add(series);
+    }
 }
