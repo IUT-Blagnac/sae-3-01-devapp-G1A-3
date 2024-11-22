1d36e76b342e554c6223218d9cd7ea4a7f5d5baa
Pierre Cornu <161845979+PierreCornu@users.noreply.github.com>
2024-11-22 14:24:02 +0100

    Update clientMQTT.py

--- Code/Python/clientMQTT.py
+++ Code/Python/clientMQTT.py
@@ -1,4 +1,3 @@
-import glob
 import paho.mqtt.client as mqtt
 import configparser
 import json
@@ -42,13 +41,6 @@ def on_message(client, userdata, msg):
         data = payload
 
     if flux_mqtt=="solaredge":
-        liste_fichier=glob.glob(os.path+'\\*')
-        last_file=liste_fichier[-1]
-        tab1=last_file.split('_').split('-')
-        ##Gérer les périodes ici
-        if liste_fichier:
-            return
-        
         if SOLAREDGE_INFO_TYPES[0] == "all" :
             message = f"{data}"
         else:
