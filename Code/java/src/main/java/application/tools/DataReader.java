6a2aa9e43992e58e3623625c9bbb0d7ad22c0937
NariaReynhard <nariareynhard@gmail.com>
2024-11-26 16:56:14 +0100

    progression sur l'affichage des données et début d'implémentation de thread

--- Code/java/src/main/java/application/tools/DataReader.java
+++ Code/java/src/main/java/application/tools/DataReader.java
@@ -22,6 +22,20 @@ public class DataReader {
         return tempsToReturn;
     }
 
+    public static float getTemps(File datasToFetch){
+        //-275 étant inférieur au 0 absolu, c'est une valeur absurde qui sera détectable de manière automatique
+        float tempToReturn = -275;
+        try {
+            String data = Files.readString(datasToFetch.toPath());
+            data = data.replace("{","").replace("}","");
+            String[] splittedValues = data.split(", ");
+            tempToReturn = Float.parseFloat(splittedValues[0]);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return tempToReturn;
+    }
+
     public static List<Float> getHumidities(List<File> datasToFetch){
         List <Float> humiditiesToReturn = new ArrayList<>();
         for(File currentFile : datasToFetch){
@@ -37,6 +51,19 @@ public class DataReader {
         return humiditiesToReturn;
     }
 
+    public static float getHumidities(File datasToFetch){
+        float humidityToReturn = -1;
+        try {
+            String data = Files.readString(datasToFetch.toPath());
+            data = data.replace("{","").replace("}","");
+            String[] splittedValues = data.split(", ");
+            humidityToReturn = Float.parseFloat(splittedValues[1]);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return humidityToReturn;
+    }
+
     public static List<Float> getCo2(List<File> datasToFetch){
         List <Float> co2ToReturn = new ArrayList<>();
         for(File currentFile : datasToFetch){
@@ -48,6 +75,19 @@ public class DataReader {
             } catch(IOException e){
                 throw new RuntimeException(e);
             }
+        }
+        return co2ToReturn;
+    }
+
+    public static float getCo2(File datasToFetch){
+        float co2ToReturn = -1;
+        try {
+            String data = Files.readString(datasToFetch.toPath());
+            data = data.replace("{","").replace("}","");
+            String[] splittedValues = data.split(", ");
+            co2ToReturn = Float.parseFloat(splittedValues[2]);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
         return co2ToReturn;
     }
