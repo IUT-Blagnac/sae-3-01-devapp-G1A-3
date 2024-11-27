dd2a1dcc2b2adf25d7969a499505624e9873c62d
RaphaLLamothe <raphael.lamothe@etu.univ-tlse2.fr>
2024-11-27 12:07:26 +0100

    VESRION FINIE ANCIENNES DONNEES

--- Code/java/src/main/java/application/view/DonneesAnterieuresController.java
+++ Code/java/src/main/java/application/view/DonneesAnterieuresController.java
@@ -3,6 +3,7 @@ package application.view;
 import java.io.File;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import application.control.IoTMainFrame;
@@ -24,9 +25,18 @@ public class DonneesAnterieuresController {
 
     private Stage containingStage;
     private IoTMainFrame main;
+
     private List<String> choix = new ArrayList<String>();
     private LocalDate dateDebut;
     private LocalDate dateFin;
+
+    private int compteNbSalles = 0;
+    private int ancienNbSalles = 0;
+    private LineChart<Number, Number> graphCO2;
+    private LineChart<Number, Number> graphTemp;
+    private LineChart<Number, Number> graphHum;
+
+    private HashMap<String, XYChart.Series<Number, Number>> seriesMap = new HashMap<>();
 
     @FXML
     VBox contenu;
@@ -49,7 +59,8 @@ public class DonneesAnterieuresController {
             alert.show();
             main.choixTypeDonneesAnterieures(containingStage);
         } else {
-            affichage(choix, dateDebut, dateFin);
+            menu(choix, dateDebut, dateFin);
+            creerGraphiques(choix);
             this.containingStage.show();
         }
     }
@@ -90,7 +101,82 @@ public class DonneesAnterieuresController {
         main.choixTypeDonneesAnterieures(containingStage);
     }
 
-    private void actualisationData(LocalDate dateDebut, LocalDate dateFin, String nomFichier, List<String> choix){
+
+
+    public void menu(List<String> choix, LocalDate dateDebut, LocalDate dateFin){
+
+        File dossier = new File("Code/Java/src/main/resources/application/capteur/AM107");
+
+        final String sMenuTextStart = "Salle";
+        final MenuButton choices = new MenuButton(sMenuTextStart);
+        final List<CheckMenuItem> items = new ArrayList<>();
+        
+
+        for (String name : dossier.list()){
+            CheckMenuItem bouton = new CheckMenuItem(name);
+            bouton.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent e) {
+                    if (bouton.isSelected()) {
+                        ajouteData(dateDebut, dateFin, name, choix);
+                        ancienNbSalles = compteNbSalles;
+                        compteNbSalles ++;
+                    }
+                    else{
+                        supprData(choix, name);
+                        ancienNbSalles = compteNbSalles;
+                        compteNbSalles --;
+                    }
+                    if (compteNbSalles == 0){
+                        retireGraphiques();
+                    }
+                    if (compteNbSalles == 1 && ancienNbSalles == 0){
+                        ajouteGraphiques(choix);
+                    }
+                }
+            });
+            items.add(bouton);
+        }
+        choices.getItems().addAll(items);
+        contenu.getChildren().add(choices);
+    }
+
+
+
+    public void creerGraphiques(List<String> choix){
+
+        NumberAxis xAxis = new NumberAxis(0, 24, 1);
+
+        if (choix.contains("CO2")){
+            NumberAxis yAxis = new NumberAxis(0, 5000, 250); 
+            yAxis.setLabel("CO2");
+            graphCO2 = new LineChart<>(xAxis, yAxis);
+        }
+        if (choix.contains("Temperature")){
+            NumberAxis yAxis = new NumberAxis(0, 100, 10); 
+            yAxis.setLabel("Température");
+            graphTemp = new LineChart<>(xAxis, yAxis);
+        }
+        if (choix.contains("Humidite")){
+            NumberAxis yAxis = new NumberAxis(0, 100, 10); 
+            yAxis.setLabel("Humidité");
+            graphHum = new LineChart<>(xAxis, yAxis);
+        }
+    }
+
+
+
+    public void retireGraphiques(){
+        int taille = contenu.getChildren().size();
+        if (taille > 1){
+            for (int i = 1; i < taille; i++){
+                contenu.getChildren().remove(1);
+            }
+        }
+    }
+
+
+
+    private List<File> trouveFichiers(String nomFichier){
         int i = 0;
         boolean fini = false;
         String chemin = "Code/Java/src/main/resources/application/capteur/AM107/" + nomFichier;
@@ -120,75 +206,121 @@ public class DonneesAnterieuresController {
             }
             i++;
         }
-        List<Float> listeVal = DataReader.getCo2(listeFichiersBonnesDates);
-        List<XYChart.Data<Number, Number>> listeData = new ArrayList<>();
+        return listeFichiersBonnesDates;
+    }
 
-        XYChart.Series<Number, Number> series = new XYChart.Series<>();
-        for (i = 0; i < listeVal.size(); i ++){
-            float heure = Integer.parseInt(listeFichiersBonnesDates.get(i).getName().split("_")[1].split("-")[0]);
-            float minute = Integer.parseInt(listeFichiersBonnesDates.get(i).getName().split("_")[1].split("-")[1]);
-            listeData.add(new Data<Number,Number>(heure + minute/60, listeVal.get(i)));
+    
+
+    private void ajouteData(LocalDate dateDebut, LocalDate dateFin, String nomFichier, List<String> choix){
+
+        List<File> listeFichiers = trouveFichiers(nomFichier);
+
+        List<Float> listeVal = new ArrayList<>();
+
+        List<LineChart.Data<Number, Number>> listeData = new ArrayList<>();
+        List<LineChart.Series<Number,    Number>> listeSeries = new ArrayList<>();
+
+        if (choix.contains("CO2")){
+            listeData.clear();
+            listeVal = DataReader.getCo2(listeFichiers);
+
+            for (int i = 0; i < listeVal.size(); i ++){
+                float heure = Integer.parseInt(listeFichiers.get(i).getName().split("_")[1].split("-")[0]);
+                float minute = Integer.parseInt(listeFichiers.get(i).getName().split("_")[1].split("-")[1]);
+                listeData.add(new Data<Number,Number>(heure + minute/60, listeVal.get(i)));
+            }
+
+            LineChart.Series<Number,Number> serie = new LineChart.Series<>();
+            serie.setName(nomFichier);
+            serie.getData().addAll(listeData);
+            listeSeries.add(serie);
+
+            seriesMap.put(nomFichier + "-CO2", serie);
         }
-        series.getData().addAll(listeData);
-        affichage(choix, dateDebut, dateFin);
+        if (choix.contains("Temperature")){
+            listeData.clear();
+            listeVal = DataReader.getTemps(listeFichiers);
+
+            for (int i = 0; i < listeVal.size(); i ++){
+                float heure = Integer.parseInt(listeFichiers.get(i).getName().split("_")[1].split("-")[0]);
+                float minute = Integer.parseInt(listeFichiers.get(i).getName().split("_")[1].split("-")[1]);
+                listeData.add(new Data<Number,Number>(heure + minute/60, listeVal.get(i)));
+            }
+
+            LineChart.Series<Number,Number> serie = new LineChart.Series<>();
+            serie.setName(nomFichier);
+            serie.getData().addAll(listeData);
+            listeSeries.add(serie);
+            seriesMap.put(nomFichier + "-Temp", serie);
+        }
+        if (choix.contains("Humidite")){
+            listeData.clear();
+            listeVal = DataReader.getHumidites(listeFichiers);
+
+            for (int i = 0; i < listeVal.size(); i ++){
+                float heure = Integer.parseInt(listeFichiers.get(i).getName().split("_")[1].split("-")[0]);
+                float minute = Integer.parseInt(listeFichiers.get(i).getName().split("_")[1].split("-")[1]);
+                listeData.add(new Data<Number,Number>(heure + minute/60, listeVal.get(i)));
+            }
+
+            LineChart.Series<Number,Number> serie = new LineChart.Series<>();
+            serie.setName(nomFichier);
+            serie.getData().addAll(listeData);
+            listeSeries.add(serie);
+
+            seriesMap.put(nomFichier + "-Hum", serie);
+        }
+        ajouteValgraphiques(choix, dateDebut, dateFin, listeSeries);
     }
 
 
 
-    public void affichage(List<String> choix, LocalDate dateDebut, LocalDate dateFin){
-        File dossier = new File("Code/Java/src/main/resources/application/capteur/AM107");
-
-        final String sMenuTextStart = "Salle";
-        final MenuButton choices = new MenuButton(sMenuTextStart);
-        final List<CheckMenuItem> items = new ArrayList<>();
-
-        for (String name : dossier.list()){
-            CheckMenuItem bouton = new CheckMenuItem(name);
-            bouton.setOnAction(new EventHandler<ActionEvent>() {
-                public void handle(ActionEvent e) {
-                    actualisationData(dateDebut, dateFin, name, choix);
-                }
-            });
-            items.add(bouton);
-        }
-
-        choices.getItems().addAll(items);
-        contenu.getChildren().add(choices);
-        
-        NumberAxis xAxis = new NumberAxis(0, 24, 1); 
-        
+    private void supprData(List<String> choix, String nomFichier){
+        System.out.println(seriesMap);
         if (choix.contains("CO2")){
-            NumberAxis yAxis = new NumberAxis(0, 1500, 100); 
-            yAxis.setLabel("CO2");
-            LineChart<Number, Number> graph = new LineChart<>(xAxis, yAxis);
-
-            graph.getXAxis().setLabel(xAxis.getLabel());
-            graph.getYAxis().setLabel(yAxis.getLabel());
-
-            graph.getData().clear();
-            contenu.getChildren().add(graph);
+            graphCO2.getData().remove(seriesMap.get(nomFichier + "-CO2"));
+            seriesMap.remove(nomFichier + "-CO2");
         }
         if (choix.contains("Temperature")){
-            NumberAxis yAxis = new NumberAxis(0, 40, 5); 
-            yAxis.setLabel("Température");
-            LineChart<Number, Number> graph = new LineChart<>(xAxis, yAxis);
-
-            graph.getXAxis().setLabel(xAxis.getLabel());
-            graph.getYAxis().setLabel(yAxis.getLabel());
-
-            graph.getData().clear();
-            contenu.getChildren().add(graph);
+            graphTemp.getData().remove(seriesMap.get(nomFichier + "-Temp"));
+            seriesMap.remove(nomFichier + "-Temp");
         }
         if (choix.contains("Humidite")){
-            NumberAxis yAxis = new NumberAxis(0, 100, 10); 
-            yAxis.setLabel("Humidité");
-            LineChart<Number, Number> graph = new LineChart<>(xAxis, yAxis);
+            graphHum.getData().remove(seriesMap.get(nomFichier + "-Hum"));
+            seriesMap.remove(nomFichier + "-Hum");
+        }
+    }
 
-            graph.getXAxis().setLabel(xAxis.getLabel());
-            graph.getYAxis().setLabel(yAxis.getLabel());
 
-            graph.getData().clear();
-            contenu.getChildren().add(graph);
+
+    public void ajouteValgraphiques(List<String> choix, LocalDate dateDebut, LocalDate dateFin, List<LineChart.Series<Number, Number>> listeSeries){
+        int parcoursSerie = 0;
+        
+        if (choix.contains("CO2")){
+            graphCO2.getData().add(listeSeries.get(parcoursSerie));
+            parcoursSerie ++;
+        }
+        if (choix.contains("Temperature")){
+            graphTemp.getData().add(listeSeries.get(parcoursSerie));
+            parcoursSerie ++;
+        }
+        if (choix.contains("Humidite")){
+            graphHum.getData().add(listeSeries.get(parcoursSerie));
+            parcoursSerie ++;
+        }
+    }
+
+
+    
+    public void ajouteGraphiques(List<String> choix){
+        if (choix.contains("CO2")){
+            contenu.getChildren().add(graphCO2);
+        }
+        if (choix.contains("Temperature")){
+            contenu.getChildren().add(graphTemp);
+        }
+        if (choix.contains("Humidite")){
+            contenu.getChildren().add(graphHum);
         }
     }
 }
