fcbb056d23b6bbe746ce45b8c44a73426749acde
DavidTRAN <davidtran31770@gmail.com>
2024-12-01 22:11:00 +0100

    Update PHP Folder
    Ajout des images pour les modes de paiements et maj de profil.php et du style

--- Code/PHP/profile.php
+++ Code/PHP/profile.php
@@ -4,11 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.9.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="website icon" type="png" href="./images/logo/logo.png">
     <link rel="stylesheet" href="./includes/style.css">
     <title>Profil Utilisateur</title>
 </head>
@@ -16,6 +14,7 @@
 <body style="background-color: #ffe4e1; font-family: 'Arial', sans-serif;">
 
     <?php include_once './includes/header.php'; ?>
+    
     <section class="profile-wrapper mt-5 mx-auto my-5" style="max-width: 900px;">
         <div class="profile-header">
             <div class="profile-cover-photo" style="background-image: url('./images/test.jpg');"></div>
@@ -87,6 +86,40 @@
             </div>
         </div>
 
+        <!-- Section Points de Fidélité -->
+        <div class="profile-footer mt-5">
+            <div class="profile-title d-flex justify-content-between align-items-center">
+                <h5 class="title">Points de Fidélité</h5>
+                <a class="profile-link" href="#">Utiliser des points</a>
+            </div>
+            <div class="profile-loyalty-info">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 mb-3">
+                        <div class="single-loyalty-info">
+                            <h6 class="loyalty-title">Points Disponibles</h6>
+                            <p class="loyalty-value">120 Points</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-6 mb-3">
+                        <div class="single-loyalty-info">
+                            <h6 class="loyalty-title">Points Utilisés</h6>
+                            <p class="loyalty-value">30 Points</p>
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-sm-12 mb-3">
+                        <div class="single-loyalty-info">
+                            <h6 class="loyalty-title">Solde des Points</h6>
+                            <p class="loyalty-value">90 Points Restants</p>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <a class="btn btn-custom" href="#">Échanger contre des réductions</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Section Cartes -->
         <div class="profile-footer mt-5">
             <div class="profile-title d-flex justify-content-between align-items-center">
                 <h5 class="title">Cartes</h5>
@@ -124,8 +157,9 @@
                 </div>
             </div>
         </div>
+
     </section>
-    
+
 </body>
 
 </html>
