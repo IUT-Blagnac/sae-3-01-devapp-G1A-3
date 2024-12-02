d97ea9798343f535b2b836e877d6d72896be02e6
DavidTranMinhAnh <davidtran31770@gmail>
2024-12-02 08:53:13 +0100

    Erreur

--- connect.inc.php
+++
@@ -1,9 +0,0 @@
-<?php 
-try{
-  $connect = new PDO ('mysql:host=localhost;dbname=R2024PHP3011;charset=UTF8','R2024PHP3011','37ewGVpb89Mv5X');
-}
-catch (PDOException $e) {
-  echo "Erreur : ".$e->getMessage()."<br/>";
-  die();
-}
-?>
